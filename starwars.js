090acb0a3f3d73bf13a463d8f71b279cb267158f
Conner Chappell <connerchappell.media@gmail.com>
2019-10-22 17:04:49 -0600

    Added filter buttons for characters

---
+++ starwars.js
@@ -0,0 +1,66 @@
+import { people } from '/assets/people.js'
+ 
+console.log("I am JavaScript running in your page!")
+ 
+let mainArea = document.querySelector("main")
+ 
+  people.forEach((person) => {
+    let personDiv = document.createElement("div")
+    let name = document.createElement("h2")
+    let gender = document.createElement("p")
+    let pic = document.createElement("img")
+ 
+    let charNum = getCharNumber(person.url)
+ 
+    name.textContent = person.name 
+    gender.textContent = person.gender
+    pic.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+ 
+    personDiv.appendChild(name)
+    personDiv.appendChild(gender)
+    personDiv.appendChild(pic)
+ 
+    mainArea.appendChild(personDiv)
+  });
+ 
+  function getCharNumber(charURL) {
+      let end = charURL.lastIndexOf("/")
+      let charID = charURL.substring(end -2, end)
+      if(charID.indexOf("/") !== -1 ) {
+         return charID.slice(1,2)
+      } else {
+          return charID
+      }
+  }
+
+  const allDivs = Array.from(document.querySelectorAll("div"))
+  const mainHeader = document.querySelector("header")
+
+  let maleButton = document.createElement("button")
+  maleButton.textContent = "Male Characters"
+  maleButton.addEventListener("click", () => {
+    femaleCharacters.forEach(charcter => {
+    let matchedDivs = allDivs.find(oneDiv => {
+      return oneDiv.firstChild.textContent === character.name
+    })
+    matchedDivelt.setAttribute("style", "display: none;")
+    })
+  })
+
+  let femaleButton = document.createElement("button")
+  femaleButton.textContent = "Female Characters"
+  femaleButton.addEventListener("click", () => {
+    maleCharacters.forEach(charcter => {
+    let matchedDivs = allDivs.find(oneDiv => {
+      return oneDiv.firstChild.textContent === character.name
+    })
+    matchedDivelt.setAttribute("style", "display: none;")
+    })
+  })
+
+  mainHeader.appendChild(maleButton)
+  mainHeader.appendChild(femaleButton)
+ 
+  const maleCharacters = people.filter(person => person.gender === "male")
+  const femaleCharacters = people.filter(person => person.gender === "female")
+  const otherCharacters = people.filter(person => person.gender !== "female" && person.gender !== "male")
